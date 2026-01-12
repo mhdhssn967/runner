6f1b2a0bf41812b3bf69772d6eb4ff7294baf841
mhdhssn967 <mhdhssn967@gmail.com>
2026-01-12 16:26:05 +0530

    fetching companny id and branch id from url

--- src/components/HUD.jsx
+++ src/components/HUD.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const HUD = ({companyId,branchId}) => {
 
-    const comm="242434"
-    const idd="3432343"
+ 
   return (
     <div style={{display:'flex',justifyContent:'space-between', backgroundColor:'#fef301',position:'fixed',width:'100vw',zIndex:'1'}}>
         <img src="./logo.png" width={'70px'} alt="" />
@@ -13,7 +12,7 @@ const HUD = ({companyId,branchId}) => {
                 <div style={{height:'10px',width:'125px',backgroundColor:'green',borderRadius:'20px'}}></div>
                 
             </div> */}
-            <p style={{fontWeight:'200',fontSize:'12px',paddingRight:'20px',color:'#a78f08'}}>{comm} , {idd}</p>
+            <p style={{fontWeight:'200',fontSize:'12px',paddingRight:'20px',color:'#a78f08'}}>{companyId} , {branchId}</p>
         </div></div>
   )
 }
